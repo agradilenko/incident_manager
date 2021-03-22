1f5d318a15b9adcdf8866f7890940764b20eedac
agradilenko <44676263+agradilenko@users.noreply.github.com>
2021-03-22 16:40:52 +0300

    Add update function and components

---
+++ frontend/src/components/modal-edit-incidents/ModalEditIncident.tsx
@@ -0,0 +1,176 @@
+import React from "react";
+import "antd/dist/antd.css";
+import { Modal, Form, Input, Select, DatePicker } from "antd";
+import moment from "moment";
+export const dateFormat = "DD.MM.YYYY";
+
+type DataType = {
+  area: string;
+  assignee?: string;
+  dateDue: object;
+  description: string;
+  name: string;
+  priority: string;
+  status: string;
+};
+
+type Incident = {
+  area: string;
+  assignee?: string;
+  dateCreated: string;
+  dateDue: string| object;
+  description: string;
+  name: string;
+  priority: string;
+  status: string;
+  __v: number;
+  _id: string;
+};
+
+interface IncidentEditFormProps {
+  visible: boolean;
+  onCreate: (data: DataType) => void;
+  onCancel: () => void;
+  userNames: any;
+  initialValues: Incident;
+}
+
+const IncidentEditForm = (props: IncidentEditFormProps) => {
+  const { visible, onCreate, onCancel, userNames, initialValues } = props;
+  const [form] = Form.useForm();
+  const { Option } = Select;
+
+  function disabledDate(current: object) {
+    // any потому что возвращается
+    // моментовский объект с очень сложной структурой
+    // Can not select days before today and today
+    return current && current < moment().endOf("day");
+  }
+
+  return (
+    <Modal
+      visible={visible}
+      title="Edit Incident"
+      okText="Edit"
+      cancelText="Cancel"
+      onCancel={onCancel}
+      onOk={() => {
+        form
+          .validateFields()
+          .then((values) => {
+            form.resetFields();
+            onCreate(values);
+          })
+          .catch((info) => {
+            console.log("Validate Failed:", info);
+          });
+      }}
+    >
+      <Form
+        form={form}
+        layout="vertical"
+        name="form_in_modal"
+        initialValues={initialValues}
+      >
+        <Form.Item
+          name="name"
+          label="Name"
+          rules={[
+            {
+              required: true,
+              message: "Please input the name of incident",
+            },
+          ]}
+        >
+          <Input maxLength={50} />
+        </Form.Item>
+        <Form.Item
+          name="description"
+          label="Description"
+          rules={[
+            {
+              required: true,
+              message: "Please input the description of incident",
+            },
+          ]}
+        >
+          <Input type="textarea" />
+        </Form.Item>
+        <Form.Item name="assignee" label="Assignee" hasFeedback>
+          <Select placeholder="Please select a person" options={userNames} />
+        </Form.Item>
+        <Form.Item
+          name="area"
+          label="Area"
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: "Please input the area of incident",
+            },
+          ]}
+        >
+          <Select placeholder="Please select an area">
+            <Option value="Bookkeeping">Bookkeeping</Option>
+            <Option value="HR">HR</Option>
+            <Option value="Technical support">Technical support</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          name="dateDue"
+          label="Due Date"
+          rules={[
+            {
+              required: true,
+              message: "Please input the end date of the incident",
+            },
+          ]}
+        >
+          <DatePicker disabledDate={disabledDate} format={dateFormat} />
+        </Form.Item>
+        <Form.Item
+          name="priority"
+          label="Priority"
+          rules={[
+            {
+              required: true,
+              message: "Please input the priority of the incident",
+            },
+          ]}
+        >
+          <Select placeholder="Please select a priority">
+            <Option value="Blocker">Blocker</Option>
+            <Option value="Critical">Critical</Option>
+            <Option value="Major">Major</Option>
+            <Option value="Normal">Minor</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          name="status"
+          label="Status"
+          rules={[
+            {
+              required: true,
+              message: "Please input status of the incident",
+            },
+          ]}
+        >
+          <Select placeholder="Please select a status">
+            <Option value="Open">Open</Option>
+            <Option value="Additional information is required">
+              Additional information is required
+            </Option>
+            <Option value="Information provided">Information provided</Option>
+            <Option value="Fixed">Fixed</Option>
+            <Option value="Checked">Checked</Option>
+            <Option value="Closed">Closed</Option>
+            <Option value="Reopened">Reopened</Option>
+            <Option value="Reject">Reject</Option>
+          </Select>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+export { IncidentEditForm };
---
+++ frontend/src/components/modal-edit-incidents/ModalEditingForm.tsx
@@ -0,0 +1,100 @@
+import React, {FunctionComponent, useEffect, useState} from "react";
+import {Button} from "antd";
+import {connect} from "react-redux";
+import {updateIncident} from "../../actions/incidentsActions";
+import {withRouter} from "react-router-dom";
+import {SimpleMap} from "../../../../simple-map";
+import {getUsersList} from "../../actions/authActions";
+import {dateFormat, IncidentEditForm} from "./ModalEditIncident";
+import {EditOutlined} from "@ant-design/icons";
+import moment from "moment";
+
+type Incident = {
+  area: string;
+  assignee?: string;
+  dateCreated: string;
+  dateDue: string | object;
+  description: string;
+  name: string;
+  priority: string;
+  status: string;
+  __v: number;
+  _id: string;
+};
+
+interface userListInterface {
+  title: string;
+  value: string;
+}
+
+interface CollectionsPageProps {
+  updateIncident: (incident: SimpleMap<any>) => void;
+  getUsersList: () => void;
+  userList: userListInterface[];
+  item: Incident
+}
+
+interface authInterface {
+  isAuthenticated: boolean;
+  userList: userListInterface[];
+}
+
+const EditIncidentFormWrapper: FunctionComponent<any> = (
+  props: CollectionsPageProps
+) => {
+  const [visible, setVisible] = useState(false);
+  const { getUsersList, userList, item, } = props;
+
+  item.dateDue = moment(item.dateDue, dateFormat);
+
+  useEffect(() => {
+    getUsersList();
+  }, []);
+
+
+  const onCreate = (values: any) => {
+    const resultData: SimpleMap<any> = {
+      ...values,
+      _id: item._id,
+      dateDue: values.dateDue.format(dateFormat),
+    };
+    props.updateIncident(resultData);
+    setVisible(false);
+  };
+
+  return (
+    <div>
+      <Button
+        type="primary"
+        onClick={() => {
+          setVisible(true);
+        }}
+        icon={<EditOutlined/>}
+      >
+      </Button>
+      <IncidentEditForm
+        initialValues={item}
+        userNames={userList}
+        visible={visible}
+        onCreate={onCreate}
+        onCancel={() => {
+          setVisible(false);
+        }}
+      />
+    </div>
+  );
+};
+
+const mapStateToProps = (state: {
+  userList: authInterface;
+  incident: Incident;
+}) => ({
+  incident: state.incident,
+  // @ts-ignore
+  userList: state.auth.usersNames,
+});
+
+export default connect(mapStateToProps, {
+  updateIncident,
+  getUsersList,
+})(withRouter(EditIncidentFormWrapper));
